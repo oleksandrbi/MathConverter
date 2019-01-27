46ea7a6ff724f43003a36634d4f8cfaac6302b7f
oleksandrbi <o.bihary@gmail.com>
2019-01-27 13:54:51 -0500

    wrong

--- Convert.cpp
+++ Convert.cpp
@@ -4,6 +4,22 @@ using namespace std;
 
 int main()
 {
-  cout << "TEST" << endl;
+  int num = 1;
+  int num1 = 1;
+  int num2 = 2;
+
+  cout << "ENTER NUMBER: " << endl;
+  int UserNum;
+  cin >> UserNum;
+
+  int NewNum = 0;
+
+  for (size_t i = 0; i < UserNum; i++)
+  {
+    NewNum = num1 + num2;
+    cout << "NewNum: " << NewNum << endl;
+    num2 = num1;
+  }
+
   return 0;
 }
